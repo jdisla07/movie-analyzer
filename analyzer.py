c3e98279b167884ffa3f0c2bb4063c7272115fd7
Julio Disla <julio@movatic.co>
2025-09-30 02:16:16 -0400

    adding movie refactor method

--- analyzer.py
+++ analyzer.py
@@ -1,2 +1,21 @@
+def refactor_movies(movie_list):
+    return [
+        {
+            "id": row[0],
+            "rating": row[1],
+            "title": row[2],
+            "certified_fresh": row[3],
+        }
+        for row in movie_list
+    ]
+
 if __name__ == '__main__':
-    print('Hello world!')
+    refactored_movies = refactor_movies(
+        movie_list=[
+            [1, 9.0, "Interstellar", True],
+            [2, 8.5, "Fast and the Furious", True],
+            [3, 7.2, "The Matrix", False],
+            [4, 9.5, "Inception", True],
+        ]
+    )
+    print(refactored_movies)
